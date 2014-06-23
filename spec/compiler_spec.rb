3ae72261645b2f68d082fee30966b84a287135af
Jonathan Hefner <jonathan.hefner@gmail.com>
2014-06-22 21:52:23 -0500

    Clean up compiler spec

--- spec/compiler_spec.rb
+++ spec/compiler_spec.rb
@@ -4,7 +4,8 @@ require 'rspec'
 
   
 describe 'Kaleidoscope.compile_run' do
-  { '1+1' => 2.0,
+  EXAMPLES = {
+    '1+1' => 2.0,
     '-1+1' => 0.0,
     '1-1' => 0.0,
     '1*2' => 2.0,
@@ -12,6 +13,7 @@ describe 'Kaleidoscope.compile_run' do
     '1/2' => 0.5,
     '2/1' => 2.0,
     '-2/-1' => 2.0,
+
     '0.5+0.5' => 1.0,
     '0.5-0.5' => 0.0,
     '2*0.5' => 1.0,
@@ -33,26 +35,34 @@ describe 'Kaleidoscope.compile_run' do
     '1>1' => 0.0,
     
     "def f() 1
-      f()" => 1.0,
+    f()" => 1.0,
+
     "def f(x) x
-      f(2)" => 2.0,
+    f(2)" => 2.0,
+
     "def f(x) x
-      f(1 + 2)" => 3.0,
+    f(1 + 2)" => 3.0,
+
     "def f(x, y) x + y
-      f(2, 3)" => 5.0,
+    f(2, 3)" => 5.0,
+
     "def f(x, y) x + y
-      f(3 + 5, 5 + 8)" => 21.0,
-    "def f(x) x + 1
-      def g(x) x - 1
-      f(g(1))" => 1.0,
-    "def f(x) x
-      def g(x) x / 2
-      f(1) > g(1)" => 1.0,
+    f(3 + 5, 5 + 8)" => 21.0,
+
+    "def f(x) x + 1 \n  def g(x) x - 1
+    f(g(1))" => 1.0,
+
+    "def f(x) x \n  def g(x) x / 2
+    f(1) > g(1)" => 1.0,
     
     "def fac(n) if n > 1 then n * fac(n - 1) else 1
-      fac(4)" => 24.0,
-    
-  }.each do |src, result|
-    it(src) { Kaleidoscope.compile_run(src).should == result }
+    fac(4)" => 24.0,
+  }
+
+  EXAMPLES.each do |src, expected|
+    it("#{src} == #{expected}") do
+      Kaleidoscope.compile_run(src).should == expected
+    end
   end
+
 end
